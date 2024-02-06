29f92abc4b3e54e3a45e5d7320ffc25498a156b5
rchielibudan <152947230+rchielibudan@users.noreply.github.com>
2024-02-06 13:21:41 +0800

    Update selectdata.php

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "webprogmi222_sf221";
+$username = "webprogmi221";
 $password = "g_6bCitLu.ljMK*m";
-$dbname = "webprogmi222_sf221";
+$dbname = "webprogmi221";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
