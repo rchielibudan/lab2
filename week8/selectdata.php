0aa06f05d24bde3ff9ea4f1e781543ea2e810113
rchielibudan <152947230+rchielibudan@users.noreply.github.com>
2024-02-06 13:19:14 +0800

    Update selectdata.php

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, name, email, website, comment, gender FROM rlibudanmyguests";
+$sql = "SELECT id, name, email, website, comment, gender FROM rlibudan_myguests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
