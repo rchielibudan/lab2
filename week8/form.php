573cd285fce60d8632d083eba8dfb2f8145aa598
rchielibudan <152947230+rchielibudan@users.noreply.github.com>
2024-02-06 12:12:30 +0800

    Update form.php

--- week8/form.php
+++ week8/form.php
@@ -101,10 +101,17 @@ echo $gender;
 ?>
 
 <?php
+// For Xampp Localhost
+//$servername = 'localhost';
+//$username = "root";
+//$password = "";
+//$dbname = "myDB";
+
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "myDB";
+$username = "webprogmi221";
+$password = "g_6bCitLu.ljMK*m";
+$dbname = "webprogmi221";
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
